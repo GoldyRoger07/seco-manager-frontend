4e51361ef3c1bab9c75bd2f26d6b97a5d127253a
GoldyRoger07 <latouche822@gmail.com>
2026-01-27 21:59:32 -0500

    9

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -7,13 +7,11 @@ import Aura from '@primeuix/themes/aura';
 import { provideHttpClient } from '@angular/common/http';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { MyPreset } from './share/mypreset';
-import { HashLocationStrategy, LocationStrategy } from '@angular/common';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideRouter(routes),
-    { provide: LocationStrategy, useClass: HashLocationStrategy },
     provideHttpClient(),
     // provideAnimationsAsync(),
     providePrimeNG({
